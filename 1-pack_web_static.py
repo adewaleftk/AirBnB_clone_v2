1fb6bac7114db93777104f4ad08cb35677c026d8
Adewale Fatoki <adewaleftk@gmail.com>
2023-06-12 08:03:48 +0100

    AirBnB Clone v2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,25 @@
+#!/usr/bin/python3
+"""Compress web static package
+"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    
-    All files in the folder web_static are added to the final archive.
-    All archives are stored in the folder versions (the function creates this folder if it doesn’t exist).
-    The name of the archive created follows the format web_static_<year><month><day><hour><minute><second>.tgz.
-    
-    Returns:
-        The path of the generated archive if successful, None otherwise.
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_path = "versions/web_static_{}.tgz".format(now)
+    """Function to compress directory
 
-    local("mkdir -p versions")
-    result = local("tar -czvf {} web_static".format(archive_path), capture=True)
+    Return: path to archive on success; None on fail
+    """
+    # Get current time
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
 
+    # Create archive
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
+
+    # Check if archiving was successful
     if result.succeeded:
         return archive_path
-    else:
-        return None
-
+    return None
